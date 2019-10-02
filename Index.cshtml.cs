cf79d43a384151e0f47d03c31cda3aee97a9eb1a
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-10-02 14:58:40 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] GoodieNs { get; set; }
+        public void OnGet()
+        {
+            GoodieNs = new string[] { "Coco Thunder", "Ghetto Delight", "Thug Sweetness", "Rough Rider" };
+        }
+    }
+}
